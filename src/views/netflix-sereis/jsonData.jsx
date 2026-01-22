3a92f2d38ed6fc4ebb60f44b74727c020ff8ccd2
Salim Ullah <itsme.salimullah@gmail.com>
2026-01-22 15:41:15 +0500

    revert changes again

--- src/views/netflix-sereis/jsonData.jsx
+++ src/views/netflix-sereis/jsonData.jsx
@@ -1,20 +1,31 @@
+import darkjpg from "../../assets/dark.jpg";
+import stranger from "../../assets/stranger.webp";
+import breaking_bad from "../../assets/breaking_bad.webp";
+import lucifer from "../../assets/lucifer.webp";
+import narcos from "../../assets/narcos.webp";
+import peaky_blinders from "../../assets/peaky_blinders.webp";
+import squid_game from "../../assets/squid_game.webp";
+import the_crown from "../../assets/the_crown.webp";
+import the_witcher from "../../assets/the_witcher.webp";
+
 const jsonData = [
   {
-    imgSrc: "/images/dark.jpg",
+    // imgSrc: "/images/dark.jpg",
+    imgSrc: darkjpg,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Dark",
     link: "https://www.netflix.com/pk/title/80100172",
   },
   {
-    imgSrc: "/images/stranger.webp",
+    imgSrc: stranger,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Stranger Things",
     link: "https://www.netflix.com/title/80057281",
   },
   {
-    imgSrc: "/images/Breaking Bad.webp",
+    imgSrc: breaking_bad,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Money Heist",
@@ -22,56 +33,56 @@ const jsonData = [
   },
 
   {
-    imgSrc: "/images/Lucifer.webp",
+    imgSrc: lucifer,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Lucifer",
     link: "https://www.netflix.com/title/80057918",
   },
   {
-    imgSrc: "/images/Narcos.webp",
+    imgSrc: narcos,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Narcos",
     link: "https://www.netflix.com/title/80025172",
   },
   {
-    imgSrc: "/images/Peaky Blinders.webp",
+    imgSrc: peaky_blinders,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Peaky Blinders",
     link: "https://www.netflix.com/title/80002479",
   },
   {
-    imgSrc: "/images/Squid Game.webp",
+    imgSrc: squid_game,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Squid Game",
     link: "https://www.netflix.com/title/81040344",
   },
   {
-    imgSrc: "/images/The Witcher.webp",
+    imgSrc: the_witcher,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "The Witcher",
     link: "https://www.netflix.com/title/80189685",
   },
   {
-    imgSrc: "/images/stranger.webp",
+    imgSrc: stranger,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Breaking Bad",
     link: "https://www.netflix.com/title/70143836",
   },
   {
-    imgSrc: "/images/The Crown.webp",
+    imgSrc: the_crown,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Peaky Blinders",
     link: "https://www.netflix.com/title/80002479",
   },
   {
-    imgSrc: "/images/The Witcher.webp",
+    imgSrc: the_witcher,
     discription:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur voluptas eos harum quas, cumque optio nihil sint velit voluptatibus vitae dicta esse facere sed fugiat enim assumenda eum temporibus consequatur!",
     title: "Locke & Key",
